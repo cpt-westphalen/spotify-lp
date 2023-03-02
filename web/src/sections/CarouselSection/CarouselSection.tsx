ecb63b2d6065c6f7fef2cf39fbaa8bcc3a2955c9
rbwestphalen <rbwestphalen@gmail.com>
2023-03-02 14:10:31 -0300

    styles: CarouselSection arrows

--- web/src/sections/CarouselSection/CarouselSection.tsx
+++ web/src/sections/CarouselSection/CarouselSection.tsx
@@ -4,10 +4,33 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "./CarouselSection.styles.css";
 
 import { FaMusic } from "react-icons/fa";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 
 export const CarouselSection = () => {
+	function renderArrowNext(handleClick: () => void, hasNext: boolean) {
+		return (
+			<button
+				disabled={!hasNext}
+				onClick={handleClick}
+				className='carousel-arrow next'
+				aria-label={"Próximo banner"}>
+				<BsChevronRight size={64} />
+			</button>
+		);
+	}
+	function renderArrowPrev(handleClick: () => void, hasNext: boolean) {
+		return (
+			<button
+				disabled={!hasNext}
+				onClick={handleClick}
+				className='carousel-arrow prev'
+				aria-label={"Próximo banner"}>
+				<BsChevronLeft size={64} />
+			</button>
+		);
+	}
 	return (
-		<div className='full-width grid-container carousel-section-bg'>
+		<section className='full-width grid-container carousel-section-bg'>
 			<div className='carousel-container'>
 				<Carousel
 					autoPlay
@@ -16,7 +39,9 @@ export const CarouselSection = () => {
 					ariaLabel='Banner do Site'
 					showThumbs={false}
 					showStatus={false}
-					interval={5000}>
+					interval={5000}
+					renderArrowNext={renderArrowNext}
+					renderArrowPrev={renderArrowPrev}>
 					<section>
 						<div>
 							<h1>Música para todos</h1>
@@ -35,6 +60,6 @@ export const CarouselSection = () => {
 					</section>
 				</Carousel>
 			</div>
-		</div>
+		</section>
 	);
 };

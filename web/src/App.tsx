3d261d954f316e482be7a572f5a16c77ce03142e
rbwestphalen <rbwestphalen@gmail.com>
2023-03-03 11:35:04 -0300

    styles: fix path to global styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { CarouselSection } from "./sections/CarouselSection/CarouselSection";

e72669172ce9f827dec14ca53586042d511e45ab
rbwestphalen <rbwestphalen@gmail.com>
2023-03-03 12:11:39 -0300

    new: footer

--- web/src/components/Footer/Footer.tsx
+++ web/src/components/Footer/Footer.tsx
@@ -1,16 +1,85 @@
 import spotify from "../../assets/spotify.svg";
 
+import socialMediaIcon1 from "../../assets/facebook.png";
+import socialMediaIcon2 from "../../assets/instagram.png";
+import socialMediaIcon3 from "../../assets/twitter.png";
+
 import "./Footer.styles.css";
 
 export const Footer = () => {
 	return (
 		<footer className='full-width grid-container'>
 			<nav>
-				<img src={spotify} />
-				<div>[coluna 1]</div>
-				<div>[coluna 2]</div>
-				<div>[coluna 3]</div>
-				<div>[social media icons]</div>
+				<div className='logo-container'>
+					<img
+						className='logo'
+						src={spotify}
+					/>
+				</div>
+				<ul
+					aria-label='links diversos'
+					className='footer-links'>
+					<li className='footer-group'>
+						<h6>{"COMPANY"}</h6>
+						<ul>
+							<li>
+								<a href='#'>Sobre</a>
+							</li>
+							<li>
+								<a href='#'>Empregos</a>
+							</li>
+							<li>
+								<a href='#'>Imprensa</a>
+							</li>
+							<li>
+								<a href='#'>Novidades</a>
+							</li>
+						</ul>
+					</li>
+
+					<li className='footer-group'>
+						<h6>{"COMUNIDADES"}</h6>
+						<ul>
+							<li>
+								<a href='#'>Artistas</a>
+							</li>
+							<li>
+								<a href='#'>Desenvolvedores</a>
+							</li>
+							<li>
+								<a href='#'>Marcas</a>
+							</li>
+						</ul>
+					</li>
+
+					<li className='footer-group'>
+						<h6>{"LINKS ÚTEIS"}</h6>
+						<ul>
+							<li>
+								<a href='#'>Ajuda</a>
+							</li>
+							<li>
+								<a href='#'>Presentes</a>
+							</li>
+							<li>
+								<a href='#'>Player na web</a>
+							</li>
+						</ul>
+					</li>
+				</ul>
+				<ul
+					className='footer-social-media'
+					aria-label='Redes sociais'>
+					<li>
+						<img src={socialMediaIcon1} />
+					</li>
+					<li>
+						<img src={socialMediaIcon2} />
+					</li>
+					<li>
+						<img src={socialMediaIcon3} />
+					</li>
+				</ul>
 			</nav>
 		</footer>
 	);

dfb95f874c02c08cb918352d390b6c7d9fdfcc1e
rbwestphalen <rbwestphalen@gmail.com>
2023-03-03 09:14:17 -0300

    feat: new article on the Gallery's left-side

---
+++ web/src/sections/GallerySection/data/articles/Extra.tsx
@@ -0,0 +1,21 @@
+export default function () {
+	return (
+		<div>
+			<h2>Esse é um artigo bônus!</h2>
+			<h3>Onde mais abriria?</h3>
+			<p>
+				{
+					"Considerando que essa galeria tem oito itens distribuídos na largura da página, a única opção para os itens à direita é abrir uma descrição à esquerda."
+				}
+			</p>
+			<h3>Detalhe</h3>
+			<p>
+				{
+					"Não necessariamente os itens precisam ter texto. Se eles não tiverem um artigo, eles não respondem ao hover."
+				}
+			</p>
+			<h3>Clicar fora</h3>
+			<p>{"Basta clicar fora desta área lateral para fechar ela."}</p>
+		</div>
+	);
+}
--- web/src/sections/GallerySection/data/galleryItems.tsx
+++ web/src/sections/GallerySection/data/galleryItems.tsx
@@ -2,13 +2,16 @@ import albumCover1 from "../../../assets/img1.jpg";
 import albumCover2 from "../../../assets/img2.jpg";
 import albumCover3 from "../../../assets/img3.jpg";
 import albumCover4 from "../../../assets/img4.jpg";
+
 import extraCover1 from "../../../assets/1.jpeg";
 import extraCover2 from "../../../assets/2.jpeg";
 import extraCover3 from "../../../assets/3.jpeg";
 import extraCover4 from "../../../assets/4.jpeg";
+
 import A0 from "./articles/Article0";
 import A1 from "./articles/Article1";
 import A2 from "./articles/Article2";
+import Extra from "./articles/Extra";
 
 export const galleryItems = [
 	{
@@ -37,7 +40,7 @@ export const galleryItems = [
 	},
 	{
 		img: { alt: "", url: extraCover3 },
-		article: null,
+		article: <Extra />,
 	},
 	{
 		img: { alt: "", url: extraCover4 },
